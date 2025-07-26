958dcaa984db3633480fee530a2feed8d6123b74
rahulkumarclg <rahul.code2024@gmail.com>
2025-07-26 20:16:41 +0530

    basics import and export command

---
+++ 01_NodeJS/math.js
@@ -0,0 +1,20 @@
+const information = "hello sir ji kya ho raha hai";
+
+
+module.exports = information;
+
+let one = 1;
+let two = 1345;
+let three = 13453;
+let four = 1456;
+let six = 345431;
+
+const objData =  {
+    one : one,
+    two : two,
+    three : three,
+    four : four,
+    six : six
+};
+
+module.exports = objData;
---
+++ 01_NodeJS/script.js
@@ -0,0 +1,6 @@
+// const data1 = require("./math");
+// console.log(data1);
+
+const datasOfObjects = require("./math");
+
+console.log(datasOfObjects)
