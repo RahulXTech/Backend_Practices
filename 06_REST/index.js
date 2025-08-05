8ce798fb56d9138f0cb4941fc52f2249677af029
rahulkumarclg <rahul.code2024@gmail.com>
2025-08-05 13:17:17 +0530

    NodeJS with SQL

--- 06_REST/index.js
+++ 06_REST/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
 
 
 app.use(express.urlencoded({extended : true}));
@@ -9,6 +10,8 @@ app.use(express.urlencoded({extended : true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 app.use(express.static(path.join(__dirname, "public")))
+app.use(methodOverride('_method'))
+
 let port = 2000;
 let posts = [
     {
@@ -54,17 +57,20 @@ app.get("/posts/:id", (req, res)=> {
     let post =  posts.find((p)=>id === p.id);
     res.render("show.ejs", {post})
 })
-app.patch("/post/:id", (req, res)=>{
+app.get("/posts/:id", (req, res)=>{
     let {id} = req.params;
     console.log(id)
     res.send("patch requist working") 
-})
-app.get("/post/:id/edit", (req, res)=>{
-    // let {id } = req.params;
-    // let post =  posts.find((p)=>id === p.id);
-    res.render("edit.ejs", {post})
-    console.log("edit page working")
-})
+}) 
+app.patch("/posts/:id/edit", (req, res)=>{
+    let {id} = req.params;
+    let post =  posts.find((p)=>id === p.id);
+    res.render("edit.ejs", { post });
+    console.log("edit page working", `${post.contant}`);
+    res.redirect("/posts")
+
+});
+
 app.listen(port, "0.0.0.0", ()=>{
     console.log(`successfully port is working on ${port}`)
-})
+});
