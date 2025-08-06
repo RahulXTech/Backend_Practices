b01cb8853819eb783dce94f8aa5bbb2d458ae68e
rahulkumarclg <rahul.code2024@gmail.com>
2025-08-06 20:37:48 +0530

    Backend connection SQL to CLI

--- 07_SQLCLASS/index.js
+++ 07_SQLCLASS/index.js
@@ -1,12 +1,34 @@
 const { faker } = require('@faker-js/faker');
-
-let  createRandomUser = ()=>{
-    return{
-        id: faker.string.uuid(),
-        username: faker.internet.username(), 
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-    }
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'sql@6825',
+    database: 'student'
+  });
+  let getUser = ()=>{
+    return[
+        faker.string.uuid(),
+        faker.internet.username(), 
+         faker.internet.email(),
+        faker.internet.password(),
+    ]
 };
-   
- console.log(createRandomUser());
+let users = [];
+for(let i = 1; i<=100; i++){
+    users.push(getUser())
+}
+  let q = "INSERT INTO users (id, username, email, password)  VALUES ?"; //it is use for multiple user
+try{
+    connection.query(q, [users], (err, result)=>{
+        if(err){
+            throw err;
+        }
+        console.log(result);
+    });
+}catch(err){
+    console.log(err);
+}
+connection.end();
+
+
