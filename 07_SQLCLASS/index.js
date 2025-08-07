bd1d759d9626bc9e5f415fa84397ccfb949cce88
rahulkumarclg <rahul.code2024@gmail.com>
2025-08-07 22:23:41 +0530

    Backend data to frontend

--- 07_SQLCLASS/index.js
+++ 07_SQLCLASS/index.js
@@ -1,11 +1,17 @@
+const express = require("express");
+const app = express();
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname, "views"));
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'sql@6825',
     database: 'student'
   });
+
   let getUser = ()=>{
     return[
         faker.string.uuid(),
@@ -14,21 +20,30 @@ const connection = mysql.createConnection({
         faker.internet.password(),
     ]
 };
-let users = [];
-for(let i = 1; i<=100; i++){
-    users.push(getUser())
-}
-  let q = "INSERT INTO users (id, username, email, password)  VALUES ?"; //it is use for multiple user
-try{
-    connection.query(q, [users], (err, result)=>{
-        if(err){
-            throw err;
-        }
-        console.log(result);
-    });
-}catch(err){
-    console.log(err);
-}
-connection.end();
+app.listen("2000",()=>{
+    console.log("app is listen on port 2000");
+})
+let q = `SELECT COUNT(*) FROM users;`
+app.get("/", (req, res)=>{
+    try{
+        connection.query(q, (err, result)=>{
+            if(err){
+                throw err;
+            }
+            console.log(result);
+            let count = result[0]['COUNT(*)'];
+            res.render("home.ejs", {count});
+        });
+    }catch(err){
+        console.log(err);
+    }
+})
+app.get("/user",(req, res)=>{
+    let q = `SELECT * FROM user`;
+        connection.query(q, (err, result)=>{
 
-
+            if(err)return res.status(500).send("Database error");   
+            let data = result; 
+            console.log(data);
+        });
+})
