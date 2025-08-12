636fa8ea140aaacc45033d3a168527ac06bdcf15
rahulkumarclg <rahul.code2024@gmail.com>
2025-08-12 22:53:00 +0530

    started mongoDB

--- 07_SQLCLASS/index.js
+++ 07_SQLCLASS/index.js
@@ -3,6 +3,10 @@ const app = express();
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 const path = require("path");
+const { Console } = require("console");
+const methodOverride = require('method-override');
+app.use(express.urlencoded({ extended: true })); // Parse form data
+app.use(methodOverride('_method'));
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname, "views"));
 const connection = mysql.createConnection({
@@ -20,9 +24,7 @@ const connection = mysql.createConnection({
         faker.internet.password(),
     ]
 };
-app.listen("2000",()=>{
-    console.log("app is listen on port 2000");
-})
+
 let q = `SELECT COUNT(*) FROM users;`
 app.get("/", (req, res)=>{
     try{
@@ -38,12 +40,48 @@ app.get("/", (req, res)=>{
         console.log(err);
     }
 })
-app.get("/user",(req, res)=>{
-    let q = `SELECT * FROM user`;
-        connection.query(q, (err, result)=>{
+app.get("/users", (req, res) => {
+    let q = `SELECT * FROM users`;
+    connection.query(q, (err, result) => {
+        if (err) return res.status(500).send("Database error is going on");
+        res.render("user.ejs", { result });
+    }); 
+});
 
-            if(err)return res.status(500).send("Database error");   
-            let data = result; 
-            console.log(data);
-        });
+app.get("/user/:id/edit",(req,res)=>{
+    let {id} = req.params;
+    let q = `SELECT * FROM users WHERE id='${id}'`;
+    connection.query(q, (err, result) => {
+        if (err) return res.status(500).send("Database error is going on edit");
+        console.log(result);
+        let user = result[0];
+        res.render("edit.ejs", {user});
+    }); 
+
 })
+
+//update route
+app.patch("/user/:id", (req, res)=> {
+    let {id} = req.params;
+    let q = `SELECT * FROM users WHERE id='${id}'`;
+    let {password : formPass, username : newUsername} = req.body;
+
+    connection.query(q, (err, result) => {
+        if (err) return res.status(500).send("Database error is going on");
+        let user = result[0];
+        if(formPass != user.password){
+            res.send("wront password")
+        }else{
+            let q2 = `UPDATE users SET username='${newUsername}' WHERE id='${id}'`;
+            connection.query(q2, (err, result)=>{
+                if(err) return res.status(500).send("update database is not working");
+                res.send(result);
+            })
+        }
+    }); 
+});
+
+app.listen("2000",()=>{
+    console.log("app is listen on port 2000");
+})
+
--- 07_SQLCLASS/schema.sql
+++ 07_SQLCLASS/schema.sql
@@ -4,3 +4,4 @@ username VARCHAR(50) UNIQUE,
 email VARCHAR(50) UNIQUE NOT NULL,
 password VARCHAR(50) NOT NULL
 );
+ 
