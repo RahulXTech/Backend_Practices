094d78638e7a52c48b11c66f066b2feacf087bb9
rahulkumarclg <rahul.code2024@gmail.com>
2025-08-15 23:32:28 +0530

    Mongo DB opratoin and basics

---
+++ 08_MONGO/index.js
@@ -0,0 +1,19 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+main()
+.then(()=>{
+    console.log("success fully working")
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+}
+
+const userSchema = new mongoose.Schema({
+  name : String,
+  email : String,
+  age : Number
+});
