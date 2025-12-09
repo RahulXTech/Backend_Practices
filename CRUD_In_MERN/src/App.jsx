b2f8db5bc9ec9b45883954569ec2e7478d1d5ac2
RahulXTech <rahul.code2024@gmail.com>
2025-12-09 22:05:41 +0530

    CRUD Operatino In MERN For practice

---
+++ CRUD_In_MERN/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='min-h-screen bg-rose-100 flex flex-col items-center md:p-4'>
+      <div className="flex justify-between items-center">
+        <h1 className='cpitalize font-bold text-white text-6xl'>Mern Crud Operation</h1>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
